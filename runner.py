6989d8b4142154f0dd43ddfa41004baebfa9c6d3
Will Masters <wmasters93@gmail.com>
2018-10-15 11:00:06 -0600

    added restart script

---
+++ runner.py
@@ -0,0 +1,10 @@
+import psutil
+import sys
+from subprocess import Popen
+
+for process in psutil.process_iter():
+    if process.cmdline() == ['python', 'bsBot.py']:
+        sys.exit('Process found: exiting.')
+
+print('Process not found: starting it.')
+Popen(['python', '~//home/ec2-user/bsSlackBot/bsBot.py'])
