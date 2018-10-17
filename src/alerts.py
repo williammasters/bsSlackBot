130214523e455d70010388e57123d9db9052b9e0
Will Masters <wmasters93@gmail.com>
2018-10-17 12:11:06 -0600

    added channel alert for restarts

--- src/alerts.py
+++ src/alerts.py
@@ -10,7 +10,7 @@ def slackAlert():
     # instantiate Slack client
     slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
     if slack_client.rtm_connect(with_team_state=False):
-        response = 'BS generator was restarted at {}'.format(datetime.datetime.now())
-        slack_client.api_call("chat.postMessage", channel=channel, text=response)
+        alert_response = 'BS generator was restarted at {}'.format(datetime.datetime.now())
+        slack_client.api_call("chat.postMessage", channel=channel, text=alert_response)
     else:
         print("Connection failed. Exception traceback printed above.")
