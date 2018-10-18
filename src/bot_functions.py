2206dfac2ab53d99a6fcfde87dfaa4107f246abd
Will Masters <wmasters93@gmail.com>
2018-10-18 10:47:21 -0600

    time specific responses

--- src/bot_functions.py
+++ src/bot_functions.py
@@ -1,5 +1,7 @@
+import datetime
 import random
 import re
+import pytz
 from faker import Faker
 
 # instantiate faker
@@ -34,30 +36,63 @@ def parse_direct_mention(message_text):
     # the first group contains the username, the second group contains the remaining message
     return (matches.group(1), matches.group(2).strip()) if matches else (None, None)
 
+def get_time():
+    tz = pytz.timezone('America/Denver')
+    current_time = datetime.datetime.now(tz).hour
+    if current_time <= 7:
+        return 'too early'
+    elif current_time >= 17:
+        return 'too late'
+    else:
+        return 'perfect'
+
 
 def handle_command(command, channel, speaker, slack_client):
     """
       Executes bot command if the command is known
     """
+    random_choice = random.randint(1, 5)
+
     # Default response is help text for the user
-    default_response = "Not sure what you mean. Try *{}*.".format(ORIGINAL_COMMAND)
+    default_response = "Not sure what you mean. Try *{}* or *{}*.".format(ORIGINAL_COMMAND, VOODO_COMMAND)
 
     # Finds and executes the given command, filling in response
     response = None
-    # This is where you start to implement more commands!
+
     if command.startswith(ORIGINAL_COMMAND):
-        response = random.choice(['You gain success when you ', 'Try to ', 'Always remember to ', 'Businesses are successful when employees ', 'An agile team is able to ']) \
+        response = random.choice(['You gain success when you ', 'Try to ', 'Always remember to ',
+                                  'Businesses are successful when employees ', 'An agile team is able to ']) \
                    + fake.bs()
+        if random_choice == 4:
+            response = 'iNsPiRe mE :mockingspongebob:'
     elif command.startswith(VOODO_COMMAND):
-        response = random.choice(['Achieve ', 'Capture ', 'Execute ', 'Over-deliver ', 'Gain ', 'Earn Wings Via ', 'Consummate ']) + fake.catch_phrase().title() + 's'
-    random_choice = random.randint(1, 5)
+        response = random.choice(['Achieve ', 'Capture ', 'Execute ', 'Over-deliver ', 'Gain ',
+                                  'Earn Wings Via ', 'Consummate ']) \
+                   + fake.catch_phrase().title() + 's'
+        if random_choice == 4:
+            response = 'sHaRe YoUr VoOdOo WiSdOm :mockingspongebob:'
+    elif command.startswith(MEANING_COMMAND):
+        response = "Synergy."
     if random_choice == 1:
         response = response + '... Betcha you feel way more productive now :wink:'
-    # Sends the response back to the channel
-    if command.startswith(MEANING_COMMAND):
-        response = "Synergy."
+    # Chris easter egg
     if speaker == "W5J6GARJ4" and random_choice == 2:
         response = "Isn't it your job to inspire us?"
+    if random_choice == 4:
+        response = 'Naw.. I don\'t really feel like it'
+
+    # Working too late or too early
+    current_time = get_time()
+    if current_time == 'too early':
+        response = random.choice(['It is too early for this, I haven\'t even had coffee yet',
+                                  'Sorry champ I\'m getting my swoll on at the gym rn',
+                                  'Are you trying to get a promotion? Why are you working so early?'])
+    elif current_time == 'too late':
+        response = random.choice(['Bro! Don\'t you know that work life balance is essential for success? You should not be working this late.',
+                                  'It\'s 5 O\'clock somewhere! Specifically here.. See you at the bar!',
+                                  'Error 404: Inspiration not found'])
+
+    # Sends the response back to the channel
     if response:
         slack_client.api_call("chat.postMessage", channel=channel, text=response)
     else:
